6c5cdab46b34071e35dc5eb9ba1d0db76dde51d1
yitianqin <120074570+yitianqin@users.noreply.github.com>
2026-03-28 22:17:50 -0400

    Add files via upload

---
+++ GameController.java
@@ -0,0 +1,95 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import model.game.Game;
+import view.GameFrame;
+
+public class GameController implements ActionListener, KeyListener {
+	private final Game game;
+	private final GameFrame view;
+
+	public GameController(Game game, GameFrame view) {
+		this.game = game;
+		this.view = view;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		handleCommand(e.getActionCommand());
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_G:
+				handleCommand("GACHA");
+				break;
+			case KeyEvent.VK_1:
+				handleCommand("ROOM0");
+				break;
+			case KeyEvent.VK_2:
+				handleCommand("ROOM1");
+				break;
+			case KeyEvent.VK_3:
+				handleCommand("ROOM2");
+				break;
+			case KeyEvent.VK_A:
+				handleCommand("ATTACK");
+				break;
+			case KeyEvent.VK_D:
+				handleCommand("DODGE");
+				break;
+			case KeyEvent.VK_Q:
+				handleCommand("ITEM0");
+				break;
+			case KeyEvent.VK_W:
+				handleCommand("ITEM1");
+				break;
+			case KeyEvent.VK_E:
+				handleCommand("ITEM2");
+				break;
+			case KeyEvent.VK_R:
+				handleCommand("ITEM3");
+				break;
+			case KeyEvent.VK_T:
+				handleCommand("ITEM4");
+				break;
+			default:
+				return;
+		}
+	}
+
+	private void handleCommand(String command) {
+		if (command == null || game.isGameOver() || game.isWin()) {
+			view.refresh();
+			return;
+		}
+
+		if ("GACHA".equals(command)) {
+			game.pullGacha();
+		} else if ("ATTACK".equals(command)) {
+			game.normalAttackMonster();
+		} else if ("DODGE".equals(command)) {
+			game.dodgeMonsterAttack();
+		} else if (command.startsWith("ROOM")) {
+			game.chooseNextRoom(Integer.parseInt(command.substring(4)));
+		} else if (command.startsWith("ITEM")) {
+			game.useItem(Integer.parseInt(command.substring(4)));
+		} else if (command.startsWith("DISCARD")) {
+			game.discardItem(Integer.parseInt(command.substring(7)));
+		}
+
+		game.updateGameStatus();
+		view.refresh();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+}
